fa03c28a4af9a654ca1f8b7fe70b5ad125f03196
Quinn Webber <webber.quinn@gmail.com>
2023-03-10 15:45:13 -0500

    Update code-for-rmd.R

--- code-for-rmd.R
+++ code-for-rmd.R
@@ -20,6 +20,25 @@ dragons <-
 dragons %>% GGally::ggpairs()
 
 
+dragons %>% 
+  select(mountainRange, site, testScore) %>% 
+  filter(!is.na(testScore)) %>% 
+  group_by(mountainRange, site) %>% 
+  summarise(average_test = mean(testScore)) %>% 
+  ggplot(aes(x = mountainRange, 
+             y = average_test, 
+             fill = mountainRange)) +
+  geom_col() +
+  scale_fill_viridis_d() +
+  theme(axis.text.x = element_text(hjust = 1, angle = 45)) +
+  facet_wrap(~ site)
+
+dragons %>% 
+  ggplot(aes(x = bodyLength, 
+             y = testScore, 
+             color = mountainRange)) + 
+  geom_point() 
+
 summary(lmer(bodyLength ~ testScore + (1|mountainRange), data = dragons))
 
 as_tibble(dragons)
